8ae0bda92d639c1bbd3eb29af7d774e8781c1fe7
keyo-oliveira <keyo.kb41@gmail.com>
2021-11-09 23:13:41 -0300

    fixing dates

--- src/services/register.service.js
+++ src/services/register.service.js
@@ -2,30 +2,38 @@ const newRegister = async (register) => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Access-Control-Allow-Headers", "Content-Type");
+  myHeaders.append("Acces-Control-AllowOrigin","*")
 
+  var DateTime = new Date()
+  
   const url = "http://127.0.0.1:5000/api/PessoasFisicas";
   const data = {
-    "username": register.userName,
-    "password": register.password,
-    "email": register.email,
     "name": register.name,
     "cep": register.cep,
     "street": register.street,
     "number": register.number,
     "district": register.district,
-    "dateCreation": Date.now(),
-    "cpf": register.cpf,
+    "dateCreation": DateTime.toJSON(),
+    "cpf": String(register.cpf),
     "rg": register.rg,
     "birthDate": register.birthDate,
+    "username": register.userName,
+    "password": register.password,
+    "email": register.email,
   };
 
   console.log(JSON.stringify(data));
 
-  await fetch(url, {
-    method: "post",
-    headers: myHeaders,
-    body: JSON.stringify(data),
-  }).then((response) => console.log(response));
+  try {
+    await fetch(url, {
+      method: "post",
+      headers: myHeaders,
+      body: JSON.stringify(data),
+    }).then((response) => console.log(response));
+  } catch (error) {
+    console.log(error)
+  }
+
 };
 
 export default newRegister;
